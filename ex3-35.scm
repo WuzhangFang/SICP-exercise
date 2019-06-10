96bae19921b7aced23e54dfd56bb31f10aeaaf63
WuzhangFang <wfang@huskers.unl.edu>
2019-06-09 21:33:38 -0700

    ex3-35

---
+++ ex3-35.scm
@@ -0,0 +1,25 @@
+;; Exercise 3.35
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+     	(if (< (get-value b) 0)
+	  (error "square less than 0: SQUARER"
+		 (get-value b))
+
+	  (set-value! a (sqrt (get-value b))))
+	(set-value! b (* (get-value a)) (get-value a))))
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me)
+    )
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+	   (process-new-value))
+	  ((eq? request 'I-lost-my-value)
+	   (process-forget-value))
+	  (else (error "Unknown request: SQUARER"
+		       request))))
+  (connect a me)
+  (connect b me)
+  me)
+
