b96560bc56c0575ab1d7eac412eea25ad14d0b30
WuzhangFang <wfang@huskers.unl.edu>
2019-06-29 23:57:56 -0700

    ex3-41.scm, ex3-42.scm

---
+++ ex3-42.scm
@@ -0,0 +1,25 @@
+;; Exercise 3.42
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+	     balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw
+	    (protected withdraw))
+	  (protected-deposit
+	    (protected deposit)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw)
+	     protected-withdraw)
+	    ((eq? m 'deposit)
+	     protected-deposit)
+	    ((eq? m 'balance)
+	     balance)
+	    (else (error "unknown request" m))))
+    dispatch)))
+; the difference is that serialize before or after dispatch
