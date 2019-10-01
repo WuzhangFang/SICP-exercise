97913dd1650170bd86f1ce3f758290e64dc1b4d9
WuzhangFang <wfang@huskers.unl.edu>
2019-09-30 23:33:05 -0500

    3.5.5.scm, ex3-81.scm

--- 3.5.5.scm
+++ 3.5.5.scm
@@ -1,5 +1,27 @@
+;; 3.5.5 Modularity of Functional Programs and Modularity of Objects
 (load "3.5.scm")
+
+; see the implementation in 3.1.2.scm without stream
+
+(define rand
+    (let ((x random-init))
+        (lambda () (set! x (rand-update x)) x)))
+
+(define (estimate-pi trials)
+    (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
+(define (cesaro-test)
+    (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+    (define (iter trials-remaining trials-passed)
+        (cond ((= trials-remaining 0) (/ trials-passed trials))
+              ((experiment) (iter (- trials-remaining 1) (+ trials-passed 1)))
+              (else (iter (- trials-remaining 1) trials-passed))))
+    (iter trials 0))
+
 ; stream version of Monte-Carlo procedure
+
 (define random-numbers
     (cons-stream random-init
                  (stream-map rand-update
@@ -16,19 +38,16 @@
         (map-successive-pairs
          f (stream-cdr (stream-cdr s)))))
 
-(define (monte-carlo experiment-stream
-                     passed
-                     failed)
+(define (monte-carlo experiment-stream passed failed)
     (define (next passed failed)
         (cons-stream
           (/ passed (+ passed failed))
           (monte-carlo
-            (stream-cdr experiment-stream)
-            passed
-            failed)))
+            (stream-cdr experiment-stream) passed failed)))
     (if (stream-car experiment-stream)
         (next (+ passed 1) failed)
         (next passed (+ failed 1))))
+
 (define pi
     (stream-map (lambda (p) (sqrt (/ 6 p)))
             (monte-carlo cesaro-stream 0 0)))
---
+++ ex3-81.scm
@@ -0,0 +1,22 @@
+;; Exercise 3.81
+
+;; Exercise 3.6
+(define (rand-update x)
+    (let ((m 127)
+          (a 32)
+          (b 28))
+        (modulo (+ (* a x) b) m)))
+
+(define rand
+    (let ((x 0))
+        (define (generate)
+            (set! x (rand-update x)) x)
+        (define (reset n)
+            (set! x n))
+        (define (dispatch m)
+            (cond ((eq? m 'generate) (generate))
+                  ((eq? m 'reset) reset)
+                  (else (error "no such method" m))))
+dispatch))
+
+;;
